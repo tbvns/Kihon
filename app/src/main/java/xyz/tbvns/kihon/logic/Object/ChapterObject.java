b6759a13eaef4c42bdd761ad8f2a4293de381dc7
____tbvns____ <tbvns601@gmail.com>
2026-02-26 11:50:06 +0100

    Fix scanlators causing the app to ignore chapters

--- app/src/main/java/xyz/tbvns/kihon/logic/Object/ChapterObject.java
+++ app/src/main/java/xyz/tbvns/kihon/logic/Object/ChapterObject.java
@@ -15,6 +15,8 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import org.codehaus.stax2.XMLInputFactory2;
 
+import javax.annotation.Nullable;
+
 @JacksonXmlRootElement(localName = "ComicInfo")
 public class ChapterObject {
     @JsonProperty("Title")
@@ -30,24 +32,35 @@ public class ChapterObject {
     public String summary;
 
     @JsonProperty("Writer")
+    @Nullable
     public String writer;
 
     @JsonProperty("Penciller")
+    @Nullable
     public String penciller;
 
     @JsonProperty("Genre")
+    @Nullable
     public String genre;
 
+    @JsonProperty("Translator")
+    @Nullable
+    public String translator;
+
     @JsonProperty("Web")
+    @Nullable
     public String web;
 
     @JacksonXmlProperty(localName = "PublishingStatusTachiyomi", namespace = "http://www.w3.org/2001/XMLSchema")
+    @Nullable
     public String publishingStatus;
 
     @JacksonXmlProperty(localName = "Categories", namespace = "http://www.w3.org/2001/XMLSchema")
+    @Nullable
     public String categories;
 
     @JacksonXmlProperty(localName = "SourceMihon", namespace = "http://www.w3.org/2001/XMLSchema")
+    @Nullable
     public String sourceMihon;
 
     @JsonIgnore
